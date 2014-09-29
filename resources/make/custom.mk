a30f9b643c2c34a0c8974f1da44180cd8a0f3ea1
Bill T. Ota <bill.ota@billo.systems>
2014-09-29 15:38:04 -0500

    Hid output from protobuf/riak compile.

--- resources/make/custom.mk
+++ resources/make/custom.mk
@@ -3,7 +3,7 @@ compile: get-deps clean-ebin
 	@which rebar.cmd >/dev/null 2>&1 && \
 	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar.cmd compile || \
 	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar compile
-	-@cd deps/riak_pb && make
+	-@cd deps/riak_pb && make &> /dev/null
 
 compile-no-deps: clean-ebin
 	@echo "Compiling only project code ..."
@@ -11,4 +11,4 @@ compile-no-deps: clean-ebin
 	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) \
 	rebar.cmd compile skip_deps=true || \
 	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar compile skip_deps=true
-	-@cd deps/riak_pb && make
+	-@cd deps/riak_pb && make &> /dev/null
