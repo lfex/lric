45baebb15381b4fbb3de22c959408cb818b6b432
Bjorn Wickstrom <bjorn@billo.systems>
2014-09-30 15:05:00 -0500

    Moved the riak_pb compile to the top.

--- resources/make/custom.mk
+++ resources/make/custom.mk
@@ -1,10 +1,10 @@
 compile: get-deps clean-ebin
+	@echo "Compiling Riak PB code ..."
+	-@cd deps/riak_pb && make erl_deps erl_compile c_compile
 	@echo "Compiling project code and dependencies ..."
 	@which rebar.cmd >/dev/null 2>&1 && \
 	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar.cmd compile || \
 	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar compile
-	@echo "Compiling Riak PB code ..."
-	-@cd deps/riak_pb && make erl_deps erl_compile c_compile
 
 compile-no-deps: clean-ebin
 	@echo "Compiling only project code ..."
